c84c75b54f99d37c0bde997665873f6340f0e9c9
Goose1909 <cep42@students.uwf.edu>
2018-04-12 10:41:38 -0500

    test

--- SessionCreationWizard/Form1.cs
+++ SessionCreationWizard/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessControler;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,8 @@ namespace SessionCreationWizard
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            IConnection con = new ConnectionControler();
+            con.openConnection();
         }
 
         private void wizardControl1_SelectedPageChanged(object sender, EventArgs e)
@@ -39,7 +41,8 @@ namespace SessionCreationWizard
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-
+            IRead read = new ConnectionControler();
+            read.DataReader("SQL CODE");
         }
 
         private void label1_Click_1(object sender, EventArgs e)
