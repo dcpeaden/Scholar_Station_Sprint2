30fb94452f61694704a1d0348b3e3fafc970e701
ryansknaggs <ryansknaggs@gmail.com>
2018-04-18 19:44:45 -0500

    Added email

---
+++ Group4_ScholarStation/EmailControler/Interfaces/IMailHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailControler
+{
+    public interface IMailHandler
+    {
+        string CancleSession(string reciversEmail, string studentName);
+
+        string CloseSession(string reciversEmail, string studentName);
+
+        string JoinSession(string reciversEmail, string studentName);
+
+    }
+}
---
+++ Group4_ScholarStation/EmailControler/SendingConditions/CloseSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailControler
+{
+    class CloseSession : ISelectedEmailType
+    {
+        private IMailControler mailControler;
+        private string body;
+
+        public CloseSession()
+        {
+            this.mailControler = new MailControler();
+            this.body = " ";
+        }
+
+        public string SelectedEmailType(string reciverEmail,  string sessionID)
+        {
+            body = "SessionID: " + sessionID + " tutoring session has been closed.";
+            return mailControler.SendEmail(reciverEmail, "Cancle Session", body);
+        }
+    }
+}
