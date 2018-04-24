7303b63f8620bcefd2215a99423df527cf6672e2
KennethDale1980 <kd31@students.uwf.edu>
2018-04-24 02:26:07 -0500

    Added ablility to mark a session as closed.

---
+++ Group4_ScholarStation/SQLHandler/QueryClasses/CloseSession.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessControler;
+using SQLHandler.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLHandler.QueryClasses
+{
+    class CloseSession : ICloseSession
+    {
+        private IUpdate update;
+
+        public CloseSession()
+        {
+            this.update = new ConnectionControler();
+        }
+
+        public void CloseOutSession(string sessionID)
+        {
+            String command = "UPDATE t_session SET ses_complete = 1 WHERE sessionId = '" + sessionID + "'";
+            update.ExecuteQueries(command);
+        }
+    }
+}
