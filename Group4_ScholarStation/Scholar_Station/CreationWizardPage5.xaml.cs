cc7823659c60b3e5a9ec770ee81edde5734e180c
Goose1909 <cep42@students.uwf.edu>
2018-04-27 16:12:58 -0500

    added close func.

--- Group4_ScholarStation/Scholar_Station/CreationWizardPage5.xaml.cs
+++ Group4_ScholarStation/Scholar_Station/CreationWizardPage5.xaml.cs
@@ -32,7 +32,10 @@ namespace Scholar_Station
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            
+            CreationWizardWindow window = new CreationWizardWindow();
+            window.Hide();
+            this.NavigationService.Navigate(window);
+            window.Close();
         }
     }
 }
